7af52d8ffc657a4ae43cd7b3e574df6088b8a8b2
ItzMari0 <msajr@outlook.com>
2022-07-26 17:52:10 -0300

    Requisito 8 Resolvido

--- src/App.js
+++ src/App.js
@@ -101,7 +101,8 @@ class App extends React.Component {
       cardRare,
       cardTrunfo,
       hasTrunfo,
-      isSaveButtonDisabled } = this.state;
+      isSaveButtonDisabled,
+      savedCards } = this.state;
 
     return (
       <div>
@@ -130,6 +131,20 @@ class App extends React.Component {
           cardRare={ cardRare }
           cardTrunfo={ cardTrunfo }
         />
+        <h1>Card List</h1>
+        { savedCards.map((card, index) => (
+          <div key={ index }>
+            <Card
+              cardName={ card.cardName }
+              cardDescription={ card.cardDescription }
+              cardAttr1={ card.cardAttr1 }
+              cardAttr2={ card.cardAttr2 }
+              cardAttr3={ card.cardAttr3 }
+              cardImage={ card.cardImage }
+              cardRare={ card.cardRare }
+              cardTrunfo={ card.cardTrunfo }
+            />
+          </div>))}
       </div>
     );
   }
